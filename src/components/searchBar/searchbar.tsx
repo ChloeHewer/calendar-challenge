e3698865ce117f96801c7d1505ac6042c159fd5d
Chloe Hewer <chloe.hewer@Nology.local>
2019-05-07 14:41:08 +0100

    tidying it up

--- src/components/searchBar/searchbar.tsx
+++ src/components/searchBar/searchbar.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
-import { ICalendarEvents } from "../../reducers/calendarReducer";
 import { connect } from "react-redux";
 import { IStore } from "../../reducers";
-import { setSearchText } from "../../reducers/filteredReducer";
+import { setSearchText } from "../../reducers/calendarReducer";
 
 export interface IOwnProps {}
 
 export interface IStateProps {
   setSearchText: (searchText: string) => void;
-  searchText: string;
 }
 
 export interface IState {}
@@ -28,15 +26,11 @@ class SearchBar extends React.Component<IOwnProps & IStateProps, IState> {
     event: React.FormEvent<HTMLInputElement>
   ) => {
     this.props.setSearchText(event.currentTarget.value);
-    // console.log(event.currentTarget.value);
-    console.log(this.props.searchText);
   };
 }
 
 const mapStateToProps = (state: IStore, props: IOwnProps) => {
-  return {
-    searchText: state.calendar.searchText
-  };
+  return {};
 };
 
 const mapDispatchToProps = { setSearchText };
